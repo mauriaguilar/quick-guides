a7ab7cc86b6add22e2a71019b79ea80e9b63f9da
Mauricio Aguilar <aguialrmauri@gmail.com>
2020-11-12 14:45:53 -0300

    Automatic -y installations

--- base_installer.sh
+++ base_installer.sh
@@ -2,13 +2,13 @@
 
 # Script to install all init software
 
-sudo apt-get update
+sudo apt-get update -y
 
-sudo apt install git terminator htop minder
+sudo apt-get install -y git terminator htop
 
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 sudo apt install ./google-chrome-stable_current_amd64.deb
 
-sudo apt-get install ffmpeg
-sudo apt-get update
-sudo apt-get install obs-studio
+sudo apt-get install -y ffmpeg
+sudo apt-get update -y
+sudo apt-get install -y obs-studio
