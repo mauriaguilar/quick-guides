c52b4b521cfb4b28e1389769c9759603a81cc3a7
Mauricio Aguilar <mauricio.aguilar@incluit.com>
2022-02-17 15:52:23 -0300

    Updating packages

--- base_installer.sh
+++ base_installer.sh
@@ -4,7 +4,10 @@
 
 sudo apt-get update -y
 
-sudo apt-get install -y git terminator htop
+sudo apt-get install -y git terminator htop gparted
+echo "email?: " && read email && git config --global user.email "$email"
+git config --global user.name "Mauricio Aguilar"
+
 
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 sudo apt install ./google-chrome-stable_current_amd64.deb
@@ -12,3 +15,5 @@ sudo apt install ./google-chrome-stable_current_amd64.deb
 sudo apt-get install -y ffmpeg
 sudo apt-get update -y
 sudo apt-get install -y obs-studio
+
+
